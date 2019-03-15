c3cf1a53e619a2719e0db2a1cc1483ce0e7066ed
Owen Allen <owenallenaz@gmail.com>
2019-03-14 17:01:46 -0700

    Converts from async to normal. Mispelled variable. Changes to multi-line to make it easier to diff future changes.

--- src/index.js
+++ src/index.js
@@ -11,17 +11,26 @@ function getTokenFromHeaders(headers) {
 	return headers.authorization.replace(/^Bearer /, "");
 }
 
-async function isCommonPassword(val){
+function isCommonPassword(val){
 	// https://en.wikipedia.org/wiki/List_of_the_most_common_passwords#cite_note-splashdata2018-10
 	// https://gizmodo.com/the-25-most-popular-passwords-of-2018-will-make-you-fee-1831052705
 	// most common passwords 8 characters or longer.
-	const commomPasswords = ["Password1", "password", "123456789", "12345678", "sunshine", "iloveyou", "princess", "football", "!@#$%^&*", "aa123456",  "password1", "qwerty123"]
+	const commonPasswords = [
+		"Password1",
+		"password",
+		"123456789",
+		"12345678",
+		"sunshine",
+		"iloveyou",
+		"princess",
+		"football",
+		"!@#$%^&*",
+		"aa123456",
+		"password1",
+		"qwerty123"
+	]
 
-	if ( commomPasswords.includes(val) === true ) {		
-		return true;
-	}
-
-	return false;
+	return commonPasswords.includes(val);
 }
 
 module.exports = {
@@ -29,6 +38,6 @@ module.exports = {
 	AuthPrefix,
 	AdminPrefix,
 	getTokenFromHeaders,
-	User,
-	isCommonPassword
+	isCommonPassword,
+	User
 }
