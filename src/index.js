7a2e7bc60b89cd5b81740d061526e3168eaf9b38
Owen Allen <owenallenaz@gmail.com>
2019-04-17 15:10:39 -0700

    Attempting to split the node and browser starting points to resolve issues with webpack.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,7 @@
-const AuthClient = require("./AuthClient");
-const AuthPrefix = require("./prefixes/AuthPrefix");
-const AdminPrefix = require("./prefixes/AdminPrefix");
+const browser = require("./browser");
 const DirectiveCheckPerm = require("./DirectiveCheckPerm");
-const User = require("./User");
-
-function getTokenFromHeaders(headers) {
-	if (!headers.authorization) {
-		return;
-	}
-	
-	return headers.authorization.replace(/^Bearer /, "");
-}
-
-function isCommonPassword(val){
-	// https://en.wikipedia.org/wiki/List_of_the_most_common_passwords#cite_note-splashdata2018-10
-	// https://gizmodo.com/the-25-most-popular-passwords-of-2018-will-make-you-fee-1831052705
-	// most common passwords 8 characters or longer.
-	const commonPasswords = [
-		"Password1",
-		"password",
-		"123456789",
-		"12345678",
-		"sunshine",
-		"iloveyou",
-		"princess",
-		"football",
-		"!@#$%^&*",
-		"aa123456",
-		"password1",
-		"qwerty123"
-	]
-
-	return commonPasswords.includes(val);
-}
 
 module.exports = {
-	AuthClient,
-	AuthPrefix,
-	AdminPrefix,
-	DirectiveCheckPerm,
-	getTokenFromHeaders,
-	isCommonPassword,
-	User
+	...browser,
+	DirectiveCheckPerm
 }
